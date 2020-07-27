7bda5995a3bc8092822da6336a3620a27f02e800
Eduard-R <edi_r@gmx.de>
2020-07-27 12:24:54 +0200

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 class Weather {
     constructor(city) {
-        this.apiKey = '';  //   <----  Enter your API-Key here...
+        this.apiKey = '';  //   <----  Enter your OpenWeatherMap API-Key here...
         this.city = city;
 
     }
